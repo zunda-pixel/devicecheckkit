4eb0eaad9b93c95d0593f341553fbecdca4aef0d
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-10-30 20:24:39 +0900

    Update ci.yml (#3)

--- Example/ExampleDeviceCheckApp/ExampleDeviceCheckApp/ContentView.swift
+++ Example/ExampleDeviceCheckApp/ExampleDeviceCheckApp/ContentView.swift
@@ -5,48 +5,48 @@
 //  Created by zunda on 2024/10/11.
 //
 
-import SwiftUI
 import DeviceCheck
 import DeviceCheckKit
-
+import SwiftUI
 
 let deviceCheck = DeviceCheck(
   httpClient: .urlSession(.shared),
   environmet: .development,
   teamId: "PU5HXZ4FZ2",
   p8KeyId: "C6K99BZ93U",
-  p8PrivateKey: Data("""
------BEGIN PRIVATE KEY-----
-MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgoM7Jt05epfNvxVEL
-8iOSEZusadXFlw8PMdzHvkGWnrOgCgYIKoZIzj0DAQehRANCAAR3poUdg7RH2yCg
-UN+nF3/VSf6SNGj6CUBezvOV6lpsTn47/Qw1g3DB4IQMcLL2m4TEyzo/XlACRp0J
-idr/RupT
------END PRIVATE KEY-----
-""".utf8)
+  p8PrivateKey: Data(
+    """
+    -----BEGIN PRIVATE KEY-----
+    MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgoM7Jt05epfNvxVEL
+    8iOSEZusadXFlw8PMdzHvkGWnrOgCgYIKoZIzj0DAQehRANCAAR3poUdg7RH2yCg
+    UN+nF3/VSf6SNGj6CUBezvOV6lpsTn47/Qw1g3DB4IQMcLL2m4TEyzo/XlACRp0J
+    idr/RupT
+    -----END PRIVATE KEY-----
+    """.utf8)
 )
 
 struct ContentView: View {
   @State var error: Error?
   @State var twoBits: TwoBits?
-  
+
   func registerDeviceToken() async throws {
     let deviceToken = try await DCDevice.current.generateToken()
     try await deviceCheck.updateTwoBits(deviceToken: deviceToken)
     print("Success Register DeviceToken")
   }
-  
+
   func fetchTwoBits() async throws {
     let deviceToken = try await DCDevice.current.generateToken()
     self.twoBits = try await deviceCheck.twoBits(deviceToken: deviceToken)
     print("Success Fetch Two Bits")
   }
-  
+
   func validateDeviceToken() async throws {
     let deviceToken = try await DCDevice.current.generateToken()
     try await deviceCheck.validateDeviceToken(deviceToken: deviceToken)
     print("Success Validate DeviceToken")
   }
-  
+
   var body: some View {
     List {
       Button("Register") {
@@ -59,7 +59,7 @@ struct ContentView: View {
           }
         }
       }
-      
+
       Button("Fetch Two Bits") {
         Task {
           do {
@@ -70,7 +70,7 @@ struct ContentView: View {
           }
         }
       }
-      
+
       Button("Validate Device Token") {
         Task {
           do {
@@ -81,13 +81,13 @@ struct ContentView: View {
           }
         }
       }
-      
+
       if let twoBits {
         Text(twoBits.lastUpdateTime.description)
         Text(twoBits.bit0.description)
         Text(twoBits.bit1.description)
       }
-      
+
       if let error {
         Text(error.localizedDescription)
       }
--- Example/ExampleDeviceCheckApp/ExampleDeviceCheckApp/ExampleDeviceCheckAppApp.swift
+++ Example/ExampleDeviceCheckApp/ExampleDeviceCheckApp/ExampleDeviceCheckAppApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct ExampleDeviceCheckAppApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
